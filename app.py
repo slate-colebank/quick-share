5b3704cda8769330803da1c10897d6157712bb6f
Slate Colebank <slatecolebank@gmail.com>
2025-11-29 15:01:05 -0800

    removed venv

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def list_files():
     return jsonify({'files': files}), 200
 
 @app.route('/download/<filename>', methods=['GET'])
-def download_file():
+def download_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 

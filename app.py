8cd13531576cafa1d005b1bbcb27d0af0b8fe67e
Slate Colebank <slatecolebank@gmail.com>
2025-11-29 14:53:13 -0800

    file list imp

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
 
@@ -29,12 +31,19 @@ def upload_file():
             file.save(filepath)
             uploaded.append(filename)
 
-        return jsonify({
-            'message': f'uploaded file(s)',
-            'files': uploaded
-        }), 200
+    return jsonify({
+        'message': f'uploaded file(s)',
+        'files': uploaded
+    }), 200
 
+@app.route('/files', methods=['GET'])
+def list_files():
+    files = os.listdir(UPLOAD_FOLDER)
+    return jsonify({'files': files}), 200
 
+@app.route('/download/<filename>', methods=['GET'])
+def download_file():
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 
 if __name__ == '__main__':
